19b1ae89a747e5fc7c5434d22085e49f4c255633
Robert Hendricks <rmasonh22@gmail.com>
2023-03-23 18:33:49 -0400

    poc -- pgba

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import SidebarLayout from './components/SidebarLayout.vue';
+</script>
+
+<template>
+<!-- <div class="container mx-auto bg-gray-200 rounded-x1 shadow border p-8 m-10" id="App">
+  <p class="texr-3x1 text-gray-700 font-bold mb-5">
+    Welcome to Tailwind getting started template
+  </p>
+  <p class="text-gray-500 text-lg">
+    Vue and Tailwind in action
+  </p>
+</div> -->
+<div>
+  <SidebarLayout />
+</div>
+
+</template>
+
+<style scoped>
+</style>
